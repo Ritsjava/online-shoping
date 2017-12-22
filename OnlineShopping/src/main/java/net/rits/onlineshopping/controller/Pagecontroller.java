aa8908cf091f67083b36803fae09e1550dc58e78
Ritesh <riteshrathore084@gmail.com>
2017-12-22 13:06:55 +0530

    assets updated

--- OnlineShopping/src/main/java/net/rits/onlineshopping/controller/Pagecontroller.java
+++ OnlineShopping/src/main/java/net/rits/onlineshopping/controller/Pagecontroller.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Pagecontroller {
 
-	@RequestMapping("/")
+	@RequestMapping(value={"/","/home"})
 	public String Show(Model model) {
 
 		model.addAttribute("greeting", "Welcome to Spring MVC");
+		model.addAttribute("clickhome","true");
+		model.addAttribute("title","Home");
 		return "page";
 	}
 }
